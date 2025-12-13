eeef1d9c3375ddb14bfb412489de11328dbbd423
saur0w <sthapliyal085@gmail.com>
2025-12-13 21:55:42 +0530

    updates

--- nine.cpp
+++ nine.cpp
@@ -1,59 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-struct Node {
-    char ch;
-    int f;
-    Node *l, *r;
-    Node(char c, int f): ch(c), f(f), l(NULL), r(NULL) {}
-};
-
-struct Cmp {
-    bool operator()(Node *a, Node *b) { return a->f > b->f; }
-};
-
-void buildCodes(Node *root, string s, map<char,string> &code) {
-    if (!root) return;
-    if (!root->l && !root->r) code[root->ch] = s;
-    buildCodes(root->l, s + "0", code);
-    buildCodes(root->r, s + "1", code);
-}
+const int INF = 1000000000;
 
 int main() {
-    string text = "huffman_example";
-    map<char,int> freq;
-    for (char c : text) freq[c]++;
+    int n = 4;
+    int g[4][4] = {
+        {0,   5,   INF, 10},
+        {INF, 0,   3,   INF},
+        {INF, INF, 0,   1},
+        {INF, INF, INF, 0}
+    };
+    int d[4][4];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            d[i][j] = g[i][j];
 
-    priority_queue<Node*, vector<Node*>, Cmp> pq;
-    for (auto &p : freq) pq.push(new Node(p.first, p.second));
-    while (pq.size() > 1) {
-        Node *a = pq.top(); pq.pop();
-        Node *b = pq.top(); pq.pop();
-        Node *c = new Node('$', a->f + b->f);
-        c->l = a; c->r = b;
-        pq.push(c);
+    for (int k = 0; k < n; k++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (d[i][k] < INF && d[k][j] < INF && d[i][k] + d[k][j] < d[i][j])
+                    d[i][j] = d[i][k] + d[k][j];
+
+    cout << "All-pairs shortest paths:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            if (d[i][j] == INF) cout << "INF ";
+            else cout << d[i][j] << " ";
+        cout << "\n";
     }
-    Node *root = pq.top();
-
-    map<char,string> code;
-    buildCodes(root, "", code);
-
-    cout << "Codes:\n";
-    for (auto &p : code) cout << p.first << " " << p.second << "\n";
-
-    string enc;
-    for (char c : text) enc += code[c];
-    cout << "Encoded: " << enc << "\n";
-
-    map<string,char> rev;
-    for (auto &p : code) rev[p.second] = p.first;
-    string cur, dec;
-    for (char b : enc) {
-        cur += b;
-        if (rev.count(cur)) {
-            dec += rev[cur];
-            cur.clear();
-        }
-    }
-    cout << "Decoded: " << dec << "\n";
 }
